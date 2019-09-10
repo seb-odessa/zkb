6cdff62b4a798420f497020228426a9239c60c4d
Your Name <you@example.com>
2019-09-10 14:21:26 +0300

    init

---
+++ src/lib/lib.rs
@@ -0,0 +1,11 @@
+
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use std::collections::HashMap;
+
+extern crate serde;
+extern crate serde_json;
+
+type Id =  HashMap<u64, String>;
+
+#cfg
