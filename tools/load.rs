1c58a29dfc09aa46ab9b5505386cd69e5b12afec
Your Name <you@example.com>
2019-10-01 10:26:00 +0300

    Changed buffer size and num of API receivers

--- tools/load.rs
+++ tools/load.rs
@@ -45,16 +45,11 @@ fn saver(src: &SegQueue<KillMail>, queue: &SegQueue<Id>, year: i32, month: u32, 
     loop {
         if let Ok(killmail) = src.pop() {
             records.push(killmail);
-            if records.len() > 50 {
+            if records.len() > 150 {
                 counter = counter + flush(&conn, &mut records);
                 print!("{:4}-{:02}-{:02} Loading {:5}/{:5}\r", year, month, day, counter, total);
                 std::io::stdout().flush().unwrap();
             }
-
-/*
-            DB::save(&conn, &killmail).expect("Failed to save killmail into DB");
-            counter = counter + 1;
-*/
         }
         if queue.is_empty() {
             break;
@@ -83,6 +78,10 @@ fn load_day_kills(year: i32, month: u32, day: u32) -> usize {
     }
     let results = SegQueue::new();
         scope(|scope| {
+        scope.spawn(|_| receiver(&tasks, &results));
+        scope.spawn(|_| receiver(&tasks, &results));
+        scope.spawn(|_| receiver(&tasks, &results));
+        scope.spawn(|_| receiver(&tasks, &results));
         scope.spawn(|_| receiver(&tasks, &results));
         scope.spawn(|_| receiver(&tasks, &results));
         scope.spawn(|_| receiver(&tasks, &results));
