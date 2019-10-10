8d8760231d65717f9107838aec57002ac116cd65
Your Name <you@example.com>
2019-10-10 10:41:49 +0300

    Added indices to the victims

--- migrations/2019-10-04-102000_killamils/up.sql
+++ migrations/2019-10-04-102000_killamils/up.sql
@@ -1,4 +1,5 @@
 -- Your SQL goes here
+.echo on
 
 CREATE TABLE IF NOT EXISTS killmails(
     killmail_id INTEGER NOT NULL PRIMARY KEY,
@@ -58,11 +59,11 @@ CREATE INDEX IF NOT EXISTS a_corporation_idx ON attackers(corporation_id);
 CREATE INDEX IF NOT EXISTS a_faction_idx     ON attackers(faction_id);
 CREATE INDEX IF NOT EXISTS a_weapon_type_idx ON attackers(weapon_type_id);
 
-CREATE INDEX IF NOT EXISTS a_ship_idx        ON victims(ship_type_id);
-CREATE INDEX IF NOT EXISTS a_alliance_idx    ON victims(alliance_id);
-CREATE INDEX IF NOT EXISTS a_character_idx   ON victims(character_id);
-CREATE INDEX IF NOT EXISTS a_corporation_idx ON victims(corporation_id);
-CREATE INDEX IF NOT EXISTS a_faction_idx     ON victims(faction_id);
+CREATE INDEX IF NOT EXISTS v_ship_idx        ON victims(ship_type_id);
+CREATE INDEX IF NOT EXISTS v_alliance_idx    ON victims(alliance_id);
+CREATE INDEX IF NOT EXISTS v_character_idx   ON victims(character_id);
+CREATE INDEX IF NOT EXISTS v_corporation_idx ON victims(corporation_id);
+CREATE INDEX IF NOT EXISTS v_faction_idx     ON victims(faction_id);
 
 CREATE INDEX IF NOT EXISTS items_idx ON items(item_type_id);
 
