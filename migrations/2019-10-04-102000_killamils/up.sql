460a3a7f17fc93a07481d8306fa4840ae28ed6b7
Your Name <you@example.com>
2019-10-11 11:27:02 +0300

    Added FK

--- migrations/2019-10-04-102000_killamils/up.sql
+++ migrations/2019-10-04-102000_killamils/up.sql
@@ -58,14 +58,17 @@ CREATE INDEX IF NOT EXISTS a_character_idx   ON attackers(character_id);
 CREATE INDEX IF NOT EXISTS a_corporation_idx ON attackers(corporation_id);
 CREATE INDEX IF NOT EXISTS a_faction_idx     ON attackers(faction_id);
 CREATE INDEX IF NOT EXISTS a_weapon_type_idx ON attackers(weapon_type_id);
+CREATE INDEX IF NOT EXISTS a_killmail_idx    ON attackers(killmail_id);
 
 CREATE INDEX IF NOT EXISTS v_ship_idx        ON victims(ship_type_id);
 CREATE INDEX IF NOT EXISTS v_alliance_idx    ON victims(alliance_id);
 CREATE INDEX IF NOT EXISTS v_character_idx   ON victims(character_id);
 CREATE INDEX IF NOT EXISTS v_corporation_idx ON victims(corporation_id);
 CREATE INDEX IF NOT EXISTS v_faction_idx     ON victims(faction_id);
+CREATE INDEX IF NOT EXISTS v_killmail_idx    ON victims(killmail_id);
 
-CREATE INDEX IF NOT EXISTS items_idx ON items(item_type_id);
+CREATE INDEX IF NOT EXISTS items_idx          ON items(item_type_id);
+CREATE INDEX IF NOT EXISTS items_killmail_idx ON items(killmail_id);
 
 
 
