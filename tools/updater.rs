8380a2b69ffae4f83efd515e0ce4baf3e03fdfc3
Your Name <you@example.com>
2019-10-18 10:33:58 +0300

    updater improved

--- tools/updater.rs
+++ tools/updater.rs
@@ -1,33 +1,49 @@
+#[macro_use]
+extern crate log;
+
 use lib::api;
 use lib::api::killmail::KillMail;
-use lib::api::zkb::Package;
-use lib::models::{DB, Connection, Hash};
+use lib::models::DB;
 use std::thread;
-
 
 fn flush(records: &Vec<KillMail>) -> Option<usize> {
     let conn = DB::connection();
     if let Some(_) = DB::save_all(&conn, &records).ok() {
+        info!("Saved to DB");
         Some(records.len())
     } else {
+        error!("Failed to save to DB");
         None
     }
 }
 
-fn main() {
+fn run_updater(timeout: u32) {
     let mut records: Vec<KillMail> = Vec::new();
-
-    while let Some(response) = api::gw::get_package("54689e7ff0b") {
-
+    while let Some(response) = api::gw::get_package("54689e7ff0b3cebfa1356bfbc9c7682c") {
+        info!("Received response from API");
         if let Some(content) = response.package {
-            println!("{}", content.id);
+            println!("{} {} {}", content.id, content.zkb.npc, content.zkb.href);
             records.push(content.killmail);
-        } else {
+        } else if records.len() > 0 {
             if let Some(count) = flush(&records) {
                 println!("Saved {} killmails", count);
                 records.clear();
             }
-            thread::sleep(std::time::Duration::from_secs(10));
+            thread::sleep(std::time::Duration::from_secs(timeout.into()));
         }
+    }
+}
+
+fn main() {
+    env_logger::init();
+    let args: Vec<_> = std::env::args().collect();
+    if 2 == args.len() {
+        let timeout: u32 = args[1]
+            .parse()
+            .expect("Can't convert first argument to the timeout");
+        run_updater(timeout);
+    } else {
+        println!("Usage:");
+        println!("\n\t {} timeout", args[0]);
     }
 }
