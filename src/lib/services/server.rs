94f0d81020dfcaf0bf30be3b466e1f07d6f10196
Your Name <you@example.com>
2020-02-25 12:07:59 +0200

    Added data function

--- src/lib/services/server.rs
+++ src/lib/services/server.rs
@@ -61,12 +61,12 @@ fn find(info: web::Path<String>, ctx: Context) -> HttpResponse {
 
 fn history(info: web::Path<(String, i32, i32)>, ctx: Context) -> HttpResponse {
     info!("/history/{:?}", info);
-    
+
     let route = &info.0;
     let id = info.1;
     let minutes = info.2;
     ctx.notify(format!("navigator/history/{}", route));
-    
+
     let body = match route.as_ref() {
         "system" => reports::History::system(&id, &minutes, &ctx),
         "region" => reports::History::region(&id, &minutes, &ctx),
@@ -78,7 +78,7 @@ fn history(info: web::Path<(String, i32, i32)>, ctx: Context) -> HttpResponse {
 }
 
 fn report(info: web::Path<(String, String, i32, i32)>, ctx: Context) -> HttpResponse {
-    info!("/report/{:?}", info);    
+    info!("/report/{:?}", info);
     let category = &info.0;
     let class = &info.1;
     let id = info.2;
@@ -139,7 +139,7 @@ fn api(info: web::Path<(String, String)>, ctx: Context) -> HttpResponse {
 fn route(info: web::Path<(String, String, String)>, ctx: Context) -> HttpResponse {
     let (route, departure, destination) = info.into_inner();
     ctx.notify("navigator/api/route");
-    let body = reports::System::route_named(route, departure, destination, &ctx);    
+    let body = reports::System::route_named(route, departure, destination, &ctx);
     wrap(body)
 }
 
@@ -195,8 +195,7 @@ pub fn run(context: Context) {
             .route("/navigator/services/{type}/{first}/{second}", web::get().to(services))
             .route("/navigator/history/{route}/{id}/{minutes}", web::get().to(history))
             .route("/navigator/report/{category}/{class}/{id}/{minutes}", web::get().to(report))
-            .route("/navigator/json/nodes/{id}", web::get().to(nodes))
-            .route("/navigator/json/edges/{id}", web::get().to(edges))
+            .route("/navigator/json/data/{id}", web::get().to(data))
     })
     .bind(address)
     .unwrap()
@@ -208,41 +207,34 @@ pub fn run(context: Context) {
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone, Default)]
 struct Node {
     id: i32,
-    label: String
+    name: String
 }
 impl Node {
-    pub fn new<S: Into<String>>(id: i32, label: S) -> Self { Self{id: id, label: label.into()} }
+    pub fn new<S: Into<String>>(id: i32, name: S) -> Self { Self{id: id, name: name.into()} }
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone, Default)]
-struct Edge {
-    from: i32,
-    to: i32
+struct Link {
+    source: i32,
+    target: i32
 }
-impl Edge {
-    pub fn new(from: i32, to: i32) -> Self { Self{from, to} }
+impl Link {
+    pub fn new(source: i32, target: i32) -> Self { Self{source, target} }
 }
 
-fn nodes(_info: web::Path<String>, _ctx: Context) -> HttpResponse {
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone, Default)]
+struct Data {
+    nodes: Vec<Node>,
+    links: Vec<Link>,
+}
 
-    let nodes = vec![Node::new(1, "Node 1"), Node::new(2, "Node 2"), Node::new(3, "Node 3"), Node::new(4, "Node 4"), Node::new(5, "Node 5")];
-    let json = serde_json::to_string(&nodes).ok().unwrap_or_default();
+fn data(_info: web::Path<String>, _ctx: Context) -> HttpResponse {
+    let mut data: Data;
+    data.nodes = vec![Node::new(1, "A"), Node::new(2, "B"), Node::new(3, "C"), Node::new(4, "D"), Node::new(5, "E")];
+    data.links = vec![Link::new(1, 2), Link::new(1, 3), Link::new(1, 4), Link::new(1, 5), Link::new(3, 5)];
+    let json = serde_json::to_string(&data).ok().unwrap_or_default();
     HttpResponse::Ok()
         .content_type("application/json; charset=UTF-8")
         .header("X-Header", "zkb")
         .body(json)
-}
-
-fn edges(_info: web::Path<String>, _ctx: Context) -> HttpResponse {
-
-    let edges = vec![
-        Edge::new(1, 3), Edge::new(1, 2),Edge::new(2, 4),Edge::new(2, 5),Edge::new(3, 2),Edge::new(3, 5),Edge::new(5, 1),
-        Edge::new(3, 1), Edge::new(2, 1),Edge::new(4, 2),Edge::new(5, 2),Edge::new(2, 3),Edge::new(5, 3),Edge::new(1, 5),
-        ];
-    let json = serde_json::to_string(&edges).ok().unwrap_or_default();
-    HttpResponse::Ok()
-        .content_type("application/json; charset=UTF-8")
-        .header("X-Header", "zkb")
-        .body(json)
-
 }
