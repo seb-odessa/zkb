60950a00bc41fd2b399f23f5064c3d2b07d89593
Your Name <you@example.com>
2020-03-05 13:33:17 +0200

    Work with activity display

--- src/lib/api/stats.rs
+++ src/lib/api/stats.rs
@@ -374,26 +374,11 @@ impl Activity {
                 var myChart = new Chart(ctx, {{
                 type: 'radar',
                 data: {{
-                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    labels: [ '0',  '1',  '2',  '3',  '4',  '5',  '6',  '7',  '8',  '9', '10', '11',
+                             '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'],
                     datasets: [{{
                         label: '{day}',
-                        data: [12, 19, 3, 5, 2, 3],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
+                        data: [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6],
                         borderWidth: 1
                     }}]
                 }},
@@ -401,7 +386,7 @@ impl Activity {
                     scales: {{
                         yAxes: [{{
                             ticks: {{
-                                beginAtZero: true
+                                beginAtZero: false
                             }}
                         }}]
                     }}
