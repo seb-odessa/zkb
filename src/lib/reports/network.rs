5cf692e320aba996970bc4dec5733f24ba6fe3bb
seb-odessa <seb@ukr.net>
2020-05-28 13:55:24 +0300

    Extend system network

--- src/lib/reports/network.rs
+++ src/lib/reports/network.rs
@@ -64,7 +64,7 @@ pub struct Edge {
     pub color: String,
 }
 impl Edge {
-    pub fn new(from: i32, to: i32) -> Self { Self{ from, to, color: String::from("Black") } }
+    pub fn new(from: i32, to: i32) -> Self { Self { from, to, color: String::from("Black") } }
 }
 impl PartialEq for Edge {
     fn eq(&self, other: &Self) -> bool {
@@ -73,17 +73,20 @@ impl PartialEq for Edge {
 }
 
 fn make_system_network(ids: &Vec<i32>, ctx: &Context, nodes: &mut HashMap<i32, Node>, deep: u32) {
-    if deep > 0 {
-        let mut neighbors = Vec::new();
-        for id in ids {
-            if !nodes.contains_key(id) {
-                if let Some(system) = reports::system::System::load(id, ctx) {
-                    let node = Node::new(system, 1, ctx);
-                    neighbors.append(&mut node.neighbors.clone());
-                    nodes.insert(*id, node);
-                }
+    let mut neighbors = Vec::new();
+    for id in ids {
+        if !nodes.contains_key(id) {
+            if let Some(system) = reports::system::System::load(id, ctx) {
+                let mut node = Node::new(system, 1, ctx);
+                if 0 == deep {
+                    node.shape = String::from("hexagon");    
+                }    
+                neighbors.append(&mut node.neighbors.clone());
+                nodes.insert(*id, node);
             }
         }
+    }
+    if deep > 0 {
         make_system_network(&neighbors, ctx, nodes, deep - 1);
     }
 }
@@ -92,8 +95,7 @@ pub fn get_system_nodes(id: &i32, deep: u32, ctx: &Context) -> HashMap<i32, Node
     let mut nodes:  HashMap<i32, Node> = HashMap::new();
     if deep > 0 {
         if let Some(system) = reports::system::System::load(id, ctx) {
-            let mut node = Node::new(system, 3, ctx);
-            node.border_width = 3;
+            let node = Node::new(system, 3, ctx);
             let neighbors = node.neighbors.clone();
             nodes.insert(node.id, node);
             make_system_network(&neighbors, ctx, &mut nodes, deep-1);
@@ -125,8 +127,7 @@ pub fn get_constellations_nodes(id: &i32, ctx: &Context) -> HashMap<i32, Node> {
     let mut neighbors = Vec::new();
     if let Some(systems) = reports::constellation::Constellation::get_systems(id, ctx) {
         for system in systems.into_iter() {
-            let mut node = Node::new(system, 3, ctx);
-            node.border_width = 3;
+            let node = Node::new(system, 3, ctx);
             neighbors.append(&mut node.neighbors.clone());
             nodes.insert(node.id, node);    
         }
@@ -134,7 +135,6 @@ pub fn get_constellations_nodes(id: &i32, ctx: &Context) -> HashMap<i32, Node> {
             if !nodes.contains_key(id) {
                 if let Some(system) = reports::system::System::load(id, ctx) {
                     let mut node = Node::new(system, 1, ctx);
-                    node.border_width = 1;
                     node.shape = String::from("hexagon");
                     nodes.insert(node.id, node);
                 }
